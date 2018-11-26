69f9f600be7a9d4fe111f7b79c3b40713e42e14d
Jaskaranbir <jaskaranbir.dhillon@gmail.com>
2018-11-26 02:46:01 -0500

    Fix Error-Codes consistency

--- model/errors.go
+++ model/errors.go
@@ -1,12 +1,12 @@
 package model
 
 // InternalError represents an error when something goes wrong, and its our fault.
-const InternalError = 1
+const InternalError int16 = 1
 
 // DatabaseError is when some operation related to Database,
 // such as insert or find, goes wrong and the task cannot proceed.
-const DatabaseError = 2
+const DatabaseError int16 = 2
 
 // UserError occurs when there's an error because of user's action.
 // An example would be providing invalid input.
-const UserError = 3
+const UserError int16 = 3
